96c3a60f0cdf100352213ca536e3c1295cb71aca
vjnagr <nagnazzi85@gmail.com>
2026-09-26 19:06:33 +0530

    Time: 1 ms (83.29%), Space: 44 MB (9.56%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,47 +1,45 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
         List<List<Integer>> list = new ArrayList<>();
-        if(root==null) return list;
+
+        if(root == null) return list;
+
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        int flag=0;
-        while(!q.isEmpty()){
-            int size= q.size();
-            List<Integer> ans = new ArrayList<>();
-            for(int i =0;i<size;i++){
-                TreeNode node = q.poll();
-                ans.add(node.val);
-                if(node.left!=null) q.offer(node.left);
-                if(node.right!=null) q.offer(node.right);
 
+        int flag = 0; // 0 -> left to right
+
+        while(!q.isEmpty()) {
+
+            int size = q.size();
+            List<Integer> temp = new ArrayList<>();
+
+            for(int i = 0; i < size; i++) {
+
+                TreeNode node = q.poll();
+
+                temp.add(node.val);
+
+                if(node.left != null)
+                    q.offer(node.left);
+
+                if(node.right != null)
+                    q.offer(node.right);
             }
-            if(flag==1){
-                Collections.reverse(ans);
-                list.add(ans);
-                flag=0;
+
+            // 🔥 AFTER processing the entire level
+            if(flag == 0) {
+                list.add(temp);
+                flag = 1;
             }
-            else{
-                list.add(ans);
-                flag=1;
+            else {
+                Collections.reverse(temp);
+                list.add(temp);
+                flag = 0;
             }
         }
-        return list;
-        
 
+        return list;
     }
 }
